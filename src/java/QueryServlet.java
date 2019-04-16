a12a265b7e8679755fb5a5665b78f81a4f72d701
Haimonti <haimonti@gmail.com>
2019-04-16 09:24:55 -0400

    Adding GCS components v1h

--- src/java/QueryServlet.java
+++ src/java/QueryServlet.java
@@ -149,7 +149,7 @@ public class QueryServlet extends HttpServlet
         	System.out.println("Did it update the current directory? "+unionFeat.directory());
         	String currFeatServer =unionFeat.directory()+File.separator+"feature_server.pl";
  		 	//String uploadFeat = "../../uploadFiles/feature_local.pl";
- 		 	String uploadFeat=bucket+File.separator+feature_local.pl;
+ 		 	String uploadFeat=bucket+File.separator+"feature_local.pl";
  		    String outFile=unionFeat.directory()+File.separator+"feature_union_v1a.pl";
  		    // you need a shell to execute a command pipeline
     		/*List<String> commands = new ArrayList<String>();
