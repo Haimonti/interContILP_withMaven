aa6b5a10290b25fef0f460f0c78f9a69294d70ed
Haimonti <haimonti@gmail.com>
2019-04-18 10:37:08 -0400

    Deploying on Dev Server - v1d

--- src/java/QueryServlet.java
+++ src/java/QueryServlet.java
@@ -63,7 +63,7 @@ public class QueryServlet extends HttpServlet
         //String appPath = request.getServletContext().getRealPath("");
         // constructs path of the directory to save uploaded file
         //String savePath = appPath + File.separator + SAVE_DIR;
-        String savePath = System.getProperty("user.dir") + File.separator + SAVE_DIR;
+        //String savePath = System.getProperty("user.dir") + File.separator + SAVE_DIR;
         //String savePath=File.separator+SAVE_DIR;
         // Allocate a output writer to write the response message into the network socket
       	PrintWriter out = response.getWriter();
@@ -115,7 +115,7 @@ public class QueryServlet extends HttpServlet
 		 // Write the file
 		 //outputChannel = gcsService.createOrReplace(fileName, instance);
     	 //copy(request.getInputStream(), Channels.newOutputStream(outputChannel));
-		 fileName=savePath + File.separator + fileName;		 
+		 /**fileName=savePath + File.separator + fileName;		 
 		 File fNew = new File(fileName);
 		 outputContent = new FileOutputStream(fNew);
 		 if (!fNew.exists()) 
@@ -129,7 +129,7 @@ public class QueryServlet extends HttpServlet
 		 }
 		 outputContent.flush();
 		 outputContent.close();
-		 fileContent.close(); 
+		 fileContent.close(); **/
  		 out.println("The uploaded file has been written on the server ....");  
  		 out.println("<br>");
  		 out.println("<br>");
